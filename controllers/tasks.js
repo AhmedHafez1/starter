432606520fa0306d06ceca746f4e9fdcc9b39b85
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-05-14 15:11:41 +0300

    all routs and controllers

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,4 +2,20 @@ const getAllTasks = (req, res) => {
   res.send('Every task');
 };
 
-module.exports = { getAllTasks };
+const getTask = (req, res) => {
+  res.send('One task');
+};
+
+const createTask = (req, res) => {
+  res.send('Create task');
+};
+
+const patchTask = (req, res) => {
+  res.send('Patch task');
+};
+
+const deleteTask = (req, res) => {
+  res.send('Delete task');
+};
+
+module.exports = { getAllTasks, createTask, getTask, patchTask, deleteTask };
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,7 +1,16 @@
 const express = require('express');
-const { getAllTasks } = require('../controllers/tasks');
+const {
+  getAllTasks,
+  createTask,
+  deleteTask,
+  getTask,
+  patchTask,
+} = require('../controllers/tasks');
+
 const router = express.Router();
 
 router.get('/', getAllTasks);
+router.post('/', createTask);
+router.route('/:id').get(getTask).patch(patchTask).delete(deleteTask);
 
 module.exports = router;
