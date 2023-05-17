d5967254ea95f79350c9be43ed61fc6618449eb5
EMEA\IBA1SF <fixed-term.ahmad.ibraheem@bosch.com>
2023-05-17 12:54:15 +0300

    configuring the connection string as enviroment variable

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-require('./db/connection');
-const tasks = require('./routes/tasks');
-const express = require('express');
+const tasks = require("./routes/tasks");
+const express = require("express");
+const connectDB = require("./db/connection");
+require("dotenv").config();
+
 const app = express();
 const port = 3000;
 
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 app.use(express.json());
-app.use('/api/v1/tasks', tasks);
+app.use("/api/v1/tasks", tasks);
 
-app.listen(port, () => console.log(`Server is listening on port ${port}`));
+const startServer = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`Server is listening on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const connectionString =
-  'mongodb+srv://Ahmed:123@freecluster.ek4ijog.mongodb.net/task_manager?retryWrites=true&w=majority';
+const connectDB = (url) => {
+  return mongoose.connect(url);
+};
 
-mongoose
-  .connect(connectionString)
-  .then(() => console.log('Db Connected Successfully'))
-  .catch((ereor) => console.log(ereor));
+module.exports = connectDB;
